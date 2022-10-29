996bc59aa2241cc82318bf9f41dc13b8b855e0d3
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2022-10-29 20:51:59 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,22 +1,19 @@
-import dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
 
-from jupyter_dash import JupyterDash
-
-
-
-
-font_awesome = "https://use.fontawesome.com/releases/v5.10.2/css/all.css" ##css design
-meta_tags = [{"name": "viewport", "content": "width=device-width"}] ##css design
-
-external_stylesheets = [meta_tags, font_awesome]
+# Connect to main app.py file
+from app import app
+from app import server
 
 
-app = JupyterDash(__name__, external_stylesheets=external_stylesheets)
+from apps import mdm
+from apps import index
+
 
 app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
     html.Div([
       html.Div([
         html.Div([
@@ -32,19 +29,74 @@ app.layout = html.Div([
           html.Div([
             html.P('Track Tools Around Saint-Gobain',style={'font-size':'45px','margin-left':'50px ','margin-top':'50px ','color':' #5A5A5A'}),
             html.P('To add small discription here ',style={'font-size':'20px','margin-left':'50px ','color':'#949494'}),
-            html.Button(dcc.Link('MDM Dashboard',
-                                 href='https://www.google.com/imgres?imgurl=https://cdn1.vectorstock.com/i/1000x1000/79/10/cartoon-color-characters-people-build-dashboard-vector-35307910.jpg&imgrefurl=https://www.vectorstock.com/royalty-free-vector/cartoon-color-characters-people-build-dashboard-vector-35307910&tbnid=68Ry4ejyR9A3BM&vet=1&docid=R0H7vV40X_u8MM&w=1000&h=820&hl=en_GB&source=sh/x/im',
-                                 target="https://www.google.com/imgres?imgurl=https://cdn1.vectorstock.com/i/1000x1000/79/10/cartoon-color-characters-people-build-dashboard-vector-35307910.jpg&imgrefurl=https://www.vectorstock.com/royalty-free-vector/cartoon-color-characters-people-build-dashboard-vector-35307910&tbnid=68Ry4ejyR9A3BM&vet=1&docid=R0H7vV40X_u8MM&w=1000&h=820&hl=en_GB&source=sh/x/im" ,
-                                 className='link'),className='btn'),
-            html.Button(dcc.Link('AP Solution Dashboard',
-                                  href='https://www.google.com/imgres?imgurl=https://cdn1.vectorstock.com/i/1000x1000/79/10/cartoon-color-characters-people-build-dashboard-vector-35307910.jpg&imgrefurl=https://www.vectorstock.com/royalty-free-vector/cartoon-color-characters-people-build-dashboard-vector-35307910&tbnid=68Ry4ejyR9A3BM&vet=1&docid=R0H7vV40X_u8MM&w=1000&h=820&hl=en_GB&source=sh/x/im',
-                                  target="https://www.google.com/imgres?imgurl=https://cdn1.vectorstock.com/i/1000x1000/79/10/cartoon-color-characters-people-build-dashboard-vector-35307910.jpg&imgrefurl=https://www.vectorstock.com/royalty-free-vector/cartoon-color-characters-people-build-dashboard-vector-35307910&tbnid=68Ry4ejyR9A3BM&vet=1&docid=R0H7vV40X_u8MM&w=1000&h=820&hl=en_GB&source=sh/x/im" ,
-                        className='link'),className='btn'),
+            html.Button(dcc.Link("MDM Dashboard", href='/apps/mdm',className='link'),id="btn1",className='togo'),
+                     dbc.Modal([
+                       dbc.ModalHeader(html.Button("HOME", id="close1",
+                                                   style={'font-size':'15px','color':'#1f2c56','border':'none','text-align':'right','flex':'1'}),
+                                      style={'background-color':'#D3D3D3'},close_button=False),
+                       dbc.ModalBody(id='body1'),],
+
+
+                       id="dashboard1",
+                       fullscreen=True,    # True, False
+
+                       ),
+            html.Button(dcc.Link("AP Solutions Dashboard", href='/apps/index',className='link'),id="btn2",className='togo'),
+                     dbc.Modal([
+                       dbc.ModalHeader(html.Button("HOME", id="close2",
+                                                   style={'font-size':'15px','color':'#1f2c56','border':'none','text-align':'right','flex':'1'}),
+                                      style={'background-color':'#D3D3D3'},close_button=False),
+                       dbc.ModalBody(id='body2'),],
+
+
+                       id="dashboard2",
+                       fullscreen=True, ),   # True, False
+
             ],className="left-column"),
           html.Div([
             html.Img(src=app.get_asset_url('main.png'),
             style={"width": "100%",},),],className="right-column")
           ],className="banner")]),])
+
+#############################################################################
+
+@app.callback(Output('body1', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname=='/apps/mdm':
+       return mdm.layout
+
+
+@app.callback(
+    Output("dashboard1", "is_open"),
+    [Input("btn1", "n_clicks"), Input("close1", "n_clicks")],
+    [State("dashboard1", "is_open")],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
+
+
+
+@app.callback(Output('body2', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname=='/apps/index':
+       return mdm.layout
+
+
+@app.callback(
+    Output("dashboard2", "is_open"),
+    [Input("btn2", "n_clicks"), Input("close2", "n_clicks")],
+    [State("dashboard2", "is_open")],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
 
 
 if __name__ == '__main__':
